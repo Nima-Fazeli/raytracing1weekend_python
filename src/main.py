ef85058062c46328c5a544db864780e26ee0b82d
Nima Fazeli <nimfazeli@gmail.com>
2024-11-30 00:03:57 -0500

    blazingly fast -- made the main go fast, though it'll change later back to slow

--- src/main.py
+++ src/main.py
@@ -16,23 +16,27 @@ if __name__ == "__main__":
         f.write(header_string)
 
         # dumb method
-        for j in range(image_height):
-            for i in range(image_width):
-                r = int(255.999 * i / (image_width - 1.0))
-                g = int(255.999 * j / (image_height - 1.0))
-                b = int(255.99 * 0.0)
-                pixel_string = f"{r} {g} 0\n"
-                f.write(pixel_string)
+        #for j in range(image_height):
+            #for i in range(image_width):
+                #r = int(255.999 * i / (image_width - 1.0))
+                #g = int(255.999 * j / (image_height - 1.0))
+                #b = int(255.99 * 0.0)
+                #pixel_string = f"{r} {g} 0\n"
+                #f.write(pixel_string)
 
         
         # go blazingly fast (not correct at the moment):
-        # r = np.linspace(0, 256)/(image_width - 1.0) * 255.999
-        # g = np.linspace(0, 256)/(image_width - 1.0) * 255.999
+        r = np.linspace(0, 256, 256)/(image_width - 1.0) * 255.999
+        g = np.linspace(0, 256, 256)/(image_width - 1.0) * 255.999
 
-        # R, G = np.meshgrid(r, g)
+        print(r.shape)
+        R, G = np.meshgrid(r, g)
+        
+        r_row = R.flatten()
+        g_row = G.flatten()
 
-        # for row in range(256 * 256):
-            # pixel_string = f"{r} {g} 0\n"
-            # f.write(pixel_string)
+        for row in range(256 * 256):
+            pixel_string = f"{int(r_row[row])} {int(g_row[row])} 0\n"
+            f.write(pixel_string)
 
     print("Image generated!")
